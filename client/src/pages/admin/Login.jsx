94eb1f3a643c8a5a78cf7af78e391f40b403b49b
asadullahbd <asadullah70209536@gmail.com>
2024-12-08 19:14:40 +0600

    aaa

--- client/src/pages/admin/Login.jsx
+++ client/src/pages/admin/Login.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("asadbd");
+  const [password, setPassword] = useState("asadbd6");
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
